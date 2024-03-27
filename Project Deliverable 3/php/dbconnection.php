28078d19993d91c924d64e80dd78c08abd56b7ea
Muskaan906 <144160328+Muskaan906@users.noreply.github.com>
2024-03-27 16:03:14 -0700

    Update dbconnection.php

--- Project Deliverable 3/php/dbconnection.php
+++ Project Deliverable 3/php/dbconnection.php
@@ -1,18 +1,10 @@
 <?php
-// $host = "localhost";
-// $database = "GPT";
-// $user = "root";
-// $password = "66060229";
 
-// $host = "localhost";
-// $database = "db_19574540";
-// $user = "19574540";
-// $password = "19574540";
+ $host = "localhost";
+ $database = "db_19574540";
+ $user = "19574540";
+ $password = "19574540";
 
-$host = "localhost";
-$database = "cosc360";
-$user = "83066985";
-$password = "83066985";
 $connection = mysqli_connect($host, $user, $password, $database);
 
 $error = mysqli_connect_error();
