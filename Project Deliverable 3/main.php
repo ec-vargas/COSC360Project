f01b9b117f48b97048b98da255733292ed17509c
Kevin Ruan <kev2003r@gmail.com>
2024-03-28 19:12:57 -0700

    pray it works

--- Project Deliverable 3/main.php
+++ Project Deliverable 3/main.php
@@ -91,8 +91,8 @@
                     require_once "php/dbconnection.php";
                     // Fetch lowest prices for products with photos
                     $sql = "SELECT p.ProductName, pr.Price, p.Photo
-                    FROM Products p
-                    INNER JOIN Prices pr ON p.ProductID = pr.ProductID
+                    FROM products p
+                    INNER JOIN prices pr ON p.ProductID = pr.ProductID
                     ORDER BY pr.Price 
                     LIMIT 10";
                     $result = mysqli_query($connection, $sql);
