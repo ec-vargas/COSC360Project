8b4d3214313b2de0504abb22957d7464f612acfa
ec-vargas <elisabeth_vargas@outlook.com>
2024-04-06 11:44:26 -0700

    unfinished changes

--- Project Deliverable 3/php/dbconnection.php
+++ Project Deliverable 3/php/dbconnection.php
@@ -1,15 +1,22 @@
 <?php
 
+<<<<<<< HEAD
  $host = "localhost";
  $database = "db_83066985";
  $user = "83066985";
  $password = "83066985";
+=======
+$host = "localhost";
+$database = "GPT";
+$user = "root";
+$password = "66060229";
+>>>>>>> 99e4c4a (unfinished changes)
 
 $connection = mysqli_connect($host, $user, $password, $database);
 
 $error = mysqli_connect_error();
 if ($error != null) {
     $output = "<p>Unable to connect to database!</p>";
-    exit ($output);
+    exit($output);
 }
 ?>
