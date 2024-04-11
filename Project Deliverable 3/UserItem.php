ca6b79f00ca753f09470469f1682b028e29124b9
Muskaan906 <144160328+Muskaan906@users.noreply.github.com>
2024-04-11 13:13:15 -0700

    Update UserItem.php
    Muskaan update

--- Project Deliverable 3/UserItem.php
+++ Project Deliverable 3/UserItem.php
@@ -1,41 +1,45 @@
-<?php
-require_once "php/dbconnection.php";
-session_start();
-$ProductID;
-if (isset ($_GET['ProductID'])) {
-    $ProductID = $_GET['ProductID'];
-}
-?>
 <!DOCTYPE html>
-<html>
-
-<head lang="en">
-    <meta charset="utf-8">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Product - User Panel</title>
     <link rel="stylesheet" href="css/reset.css" />
-    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="css/style.css" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <style>
+        /* CSS for comment styling */
+        .comment {
+            border: 1px solid #ccc;
+            padding: 10px;
+            margin-bottom: 15px;
+            border-radius: 5px;
+        }
+        .comment p {
+            margin: 5px 0;
+        }
+        .comment p.date {
+            font-size: 0.8em;
+            color: #666;
+        }
+    </style>
 </head>
-
 <body>
     <div class="container-fluid-2">
         <div class="row align-items-center">
             <div class="col">
                 <?php
-                    if (isset($_SESSION['username'])) {echo "<h1>GroceryPricer.ca</h1>";}
-                    else {echo "<h1><a href='home.html'>GroceryPricer.ca</a></h1>";}
+                    session_start();
+                    if (isset($_SESSION['username'])) {
+                        echo "<h1>GroceryPricer.ca</h1>";
+                    } else {
+                        echo "<h1><a href='home.html'>GroceryPricer.ca</a></h1>";
+                    }
                 ?>
             </div>
             <div class="col">
                 <div class="header2">
-                    <?php if (isset ($_SESSION['profile_photo'])): ?>
-                        <img src="<?php echo $_SESSION['profile_photo']; ?>" alt="User Profile Photo" class="img-thumbnail">
-                    <?php endif; ?>
-                    <button style="margin-right: 2%;" onclick="location.href='UserAccount.php'"><?php echo $_SESSION['username'];?></button>
+                    <button style="margin-right: 2%;"><?php echo $_SESSION['username'];?></button>
                     <a href="php/logout.php" style="font-size: 2em;">LogOut&nbsp;</a>
                     <a href="adminLogin.php" style="font-size: 2em;">&nbsp;Admin Login</a>
                 </div>
@@ -45,9 +49,11 @@ if (isset ($_GET['ProductID'])) {
     </div>
 
     <div class="col-6 mx-auto">
-        <h2 class="sheaders">Item Details</h2>
+ 
         <section>
             <?php
+            require_once "php/dbconnection.php";
+
             $ProductID = $_GET['ProductID'];
             $contains = $_GET['Contains'];
 
@@ -73,36 +79,15 @@ if (isset ($_GET['ProductID'])) {
         <div id="commentbox">
             <h2 id="comments">Tell us what you think?</h2>
             <textarea id="usercomment" rows="8" cols="40" placeholder="Add a comment!"></textarea>
-            <Button id="post">Post</Button>
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-            <script>
-                $(document).ready(function () {
-
-                    $("#post").click(function () {
-                        var newcomment = $("#usercomment").val();
-                        var ProductId = "<?php echo $ProductID; ?>";
-                        var Username = "<?php echo $_SESSION['username']; ?>";
-                        $.post("php/createpost.php", { Comment: newcomment, Username: Username, ProductId: ProductId }, function (response) {
-                            if (response.length == 0) {alert("Post Created.");}
-                            else {alert("Error creating post.");}
-                        });
-                    });
-
-                });
-            </script>
+            <button id="post">Post</button>
+            <div id="commentsContainer">
+                <!-- Comments will be displayed here -->
+            </div>
         </div>
-        <?php
-        $sql = "SELECT * FROM comments JOIN users on comments.UserID = users.UserID WHERE ProductId = '$ProductID'";
-        $results = mysqli_query($connection, $sql);
-        while ($row = mysqli_fetch_assoc($results)) {
-            echo "<div id='productcomments'>" . $row['Comment'] . "<br> - " . $row['Username'] . "</div>";
-        }
-        mysqli_free_result($results);
-        mysqli_close($connection);
-        ?>
+
         <form id="find-items-form" method="POST" action="itemResult.php">
             <input type="hidden" name="contains" value="<?php echo $contains; ?>">
-            <button id="BacktoSearch" type="submit onclick="location.href='itemResult.php'">Back to Results</button>
+            <button id="BacktoSearch" type="submit">Back to Results</button>
         </form>
         <hr>
     </div>
@@ -110,6 +95,54 @@ if (isset ($_GET['ProductID'])) {
     <footer>
         <p><i>Copyright &#169; 2024 Sandhu, Ruan and Vargas </i></p>
     </footer>
-</body>
 
+    <!-- JavaScript section -->
+    <script>
+    $(document).ready(function () {
+        $("#post").click(function () {
+            var newcomment = $("#usercomment").val();
+            var ProductId = "<?php echo $ProductID; ?>";
+            var Username = "<?php echo $_SESSION['username']; ?>";
+            $.post("php/createpost.php", { Comment: newcomment, Username: Username, ProductId: ProductId }, function (response) {
+                if (response.length == 0) {
+                    alert("Post Created.");
+                    fetchComments();
+                    $("#usercomment").val(''); // Clear the textarea
+                } else {
+                    alert("Error creating post.");
+                }
+            });
+        });
+
+        function fetchComments() {
+            var ProductId = "<?php echo $ProductID; ?>";
+            $.getJSON("php/fetchcomments.php", { ProductId: ProductId }, function (comments) {
+                var html = '';
+                $.each(comments, function (index, comment) {
+                    // Format the date
+                    var commentDate = new Date(comment['CommentDate']);
+                    var formattedDate = commentDate.toLocaleDateString('en-US', { 
+                        year: 'numeric', 
+                        month: 'short', 
+                        day: 'numeric', 
+                        hour: 'numeric', 
+                        minute: 'numeric', 
+                        second: 'numeric' 
+                    });
+                    // Construct HTML for comment
+                    html += '<div class="comment">';
+                    html += '<p>' + comment['Comment'] + '</p>';
+                    html += '<p>Posted by: ' + comment['Username'] + '</p>';
+                    html += '<p class="date">Date: ' + formattedDate + '</p>';
+                    html += '</div>';
+                });
+                $("#commentsContainer").html(html);
+            });
+        }
+
+        fetchComments();
+    });
+</script>
+
+</body>
 </html>
